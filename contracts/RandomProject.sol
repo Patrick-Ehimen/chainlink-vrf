958d9ea6de7b168e030a87000c5eeeb43830ecba
Patrick-Ehimen <osepatrick.ehimen77@gmail.com>
2023-02-06 16:21:56 +0100

    Update RandomProject.sol

--- contracts/RandomProject.sol
+++ contracts/RandomProject.sol
@@ -1,9 +1,10 @@
 // SPDX-License-Identifier: MIT
 
 pragma solidity ^0.8.0;
-import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
-import "@chainlink/contracts/src/v0.8/ConfirmedOwner.sol";
-import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
+
+// import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
+// import "@chainlink/contracts/src/v0.8/ConfirmedOwner.sol";
+// import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
 
 contract Randomn {
     string[] public groupOfProjects;
